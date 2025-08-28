55732515edc4de6ae3f67a805fbb2fa86c264ebb
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:59:15 +0530

    Sheet2(7).py

--- Sheet2(7).py
+++ Sheet2(7).py
@@ -1,10 +1,7 @@
-# 7. Take an integer A as input. You have to print the sum of all odd numbers in the range [1, A].
-
-
-N = int(input("Enter a number: "))
+n = int(input("Enter : "))
 sumofodd = 0
-for i in range(1, N+1):   
+for i in range(1, n+1):   
     if i % 2 != 0:       
         sumofodd += i
-print("Sum of all odd numbers from 1 to", N, "is:", sumofodd)
+print("sum of all odd number from 1 to", n, "is:", sumofodd)
 
