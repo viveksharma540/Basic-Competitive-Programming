4125ea1648eeb7e02bb10af731a4d48223247e32
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 07:02:57 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,10 +1,8 @@
-# 12. You are given 3 integer angles(in degrees), A, B, and C, of a triangle. You have to tell whether the triangle is valid or not. A triangle is valid if the sum of its angles equals 180.
-
-A =int(input("enter the first angle:\n"))
-B =int(input("enter the second angle:\n"))
-C =int(input("enter the third angle:\n"))
+A =int(input("enter the 1st angle:\n"))
+B =int(input("enter the 2nd angle:\n"))
+C =int(input("enter the 3rd angle:\n"))
 
 if A > 0 and B > 0 and C > 0 and (A + B + C == 180):
-    print("The triangle is valid")
+    print("valid triangle ")
 else:
-    print("The triangle is not valid")
+    print("invalif triangle ")
