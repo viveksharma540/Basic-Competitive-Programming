4cd18563abfddd9e77af84d1411c8b79df7dc054
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 07:08:58 +0530

    Update Q15.py

--- Q15.py
+++ Q15.py
@@ -1,12 +1,12 @@
-percentage = float(input("Enter your percentage:\n"))
+percentage = float(input("Enter :\n"))
 
 if percentage < 25:
-    print("Your grade is: D")
+    print("grade : D")
 elif percentage <= 45:
-    print("Your grade is: C")
+    print(" grade : C")
 elif percentage <= 65:
-    print("Your grade is: B")
+    print(" grade : B")
 elif percentage <= 85:
-    print("Your grade is: A")
+    print(" grade : A")
 else:
-    print("Your grade is: A+")
+    print("grade: A+")
