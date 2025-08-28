184239cdd12969a133437203246264783d0e3050
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:57:00 +0530

    Sheet2(3).py

--- Sheet2(3).py
+++ Sheet2(3).py
@@ -1,7 +1,4 @@
-# 3. Write a program to print all even numbers from 1 to N, where you have to take N as input 
-# from the user.
-
-N = int(input("Enter a number: "))
-for x in range(1, N+1):
+n = int(input("Enter : "))
+for x in range(1, n+1):
     if (x%2==0):
         print(x,end=" ")
