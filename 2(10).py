fd3179e171b6f2fbce8208a4bf7a0a1206ba7a48
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:44:29 +0530

    2(10).py

--- 2(10).py
+++ 2(10).py
@@ -1,14 +1,9 @@
-# 10. You are given an integer A as input, and you need to determine whether it is a palindrome 
-# or not. A palindrome integer is one whose digits, when reversed, result in the same number. 
-# For example, 121 is a palindrome because its reverse is also 121, but 123 is not a palindrome 
-# because its reverse is 321.Note: The given integer will not have any leading zeros. 
 
-
-N=int(input("Enter a number:"))
-original=N
+n=int(input("enter:"))
+original=n
 rev=""
-while N>0:
-    rem=N%10
+while n>0:
+    rem=n%10
     rev+=str(rem)
-    N=N//10
-print("Yes the number is palindrome!" if original==int(rev) else "No the number is not a plaindrome!")
+    n=n//10
+print(" palindrome" if original==int(rev) else "No plaindrome!")
