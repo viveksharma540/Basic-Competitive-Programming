7a41caaee5bd2e8c2ce0d8b82f98c5472cac25c1
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:47:08 +0530

    Sheet2(2).py

--- Sheet2(2).py
+++ Sheet2(2).py
@@ -1,7 +1,4 @@
 
-# 2. Write a program to print all Natural numbers from N to 1, where you have to take N as input 
-# from the user. 
-
-N=int(input("Enter a number:"))
-for i in range(N,0,-1):
+n=int(input("Enter :"))
+for i in range(n,0,-1):
     print(i,end = " ")
