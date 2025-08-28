43ee4d176395a00b95dbbe3aacff23fc940c841b
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 07:02:07 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,9 +1,7 @@
-# 11. Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers. 
-
-A =int(input("enter the first number:\n"))
-B =int(input("enter the second number:\n"))
-C =int(input("enter the third number:\n"))
-D =int(input("enter the fourth number:\n"))
-E =int(input("enter the fifth number:\n"))
+A =int(input("enter the first :\n"))
+B =int(input("enter the second:\n"))
+C =int(input("enter the third :\n"))
+D =int(input("enter the fourth:\n"))
+E =int(input("enter the fifth :\n"))
 Avg = (A+B+C+D+E)/5
-print("The Average of 5 number is :,",Avg)
+print(" averager is :,",Avg)
