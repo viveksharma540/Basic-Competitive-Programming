a96599b3a55f8b21f257dbfc51c90c385af78076
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:56:13 +0530

    Sheet2(12).py

--- Sheet2(12).py
+++ Sheet2(12).py
@@ -1,7 +1,5 @@
-# 12. You are given two integers A and B. You have to find the value of A^B.
-
-A = int(input("Enter the number A: "))
-B = int(input("Enter the number B: "))
+A = int(input("Enter A: "))
+B = int(input("Enter B: "))
 
 result = A ** B
 print("A^B =", result)
