6d674c32438340fa0879d43a609299e2b0093fc2
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 07:05:06 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,5 @@
-# 4. WAP to check if the number is divisible by 7 or if the last digit is 5. 
-
-Number = float(input("enter the number:\n"))
+Number = float(input("enter:\n"))
 if Number%7==0 and Number%10==5:
-    print("Yes,all condition are true")
+    print("Yes")
 else:
-    print("No,both condition are not matching")
+    print("not matching")
