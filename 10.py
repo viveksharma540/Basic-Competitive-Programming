3c8c33281c55be47f9485fd1ddd5345967539c49
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 07:01:25 +0530

    10.py

--- 10.py
+++ 10.py
@@ -1,8 +1,4 @@
-# 10. WAP to accept a number from 1 to 7 and display the name of the day, like 1 for Sunday, 2 for Monday, etc.
-
-# USING LISTS:
-
-days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-num = int(input("Enter a number (1-7): "))
-print(days[num-1] if 1 <= num <= 7 else "Invalid input! Enter 1-7.")
+days = ["sunday","monday","tuesday","wednesday","tyhursday","friday","saturday"]
+num = int(input("Enter (1-7): "))
+print(days[num-1] if 1 <= num <= 7 else "invalid input! Enter 1-7.")
  
