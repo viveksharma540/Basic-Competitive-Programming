549948c987c23f52bd07ed5ff63c382dd4851272
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:51:40 +0530

    Sheet2(8).py

--- Sheet2(8).py
+++ Sheet2(8).py
@@ -1,11 +1,9 @@
-# 8. Take an integer N as input and print the count of digits of that number. 
-
-N = int(input("Enter a number:\n"))
+n = int(input("Enter:\n"))
 count = 0
-if N == 0:
+if n == 0:
     count = 1
 else:
-    while N != 0:
-        N = N // 10  
+    while n != 0:
+        n = n // 10  
         count += 1
-print("Number of digits:", count)
+print("nnumber of digits:", count)
