37532715317e5042a711dcc4351b97677d2de507
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-28 06:58:39 +0530

    Sheet2(6).py

--- Sheet2(6).py
+++ Sheet2(6).py
@@ -1,9 +1,6 @@
-# 6. You are given an integer A, you need to find and return the sum of all the even numbers 
-# between 1 and A. Even numbers are those numbers that are divisible by 2. 
-
-N=int(input("Enter a number:"))
+n=int(input("Enter:"))
 sum=0
-for i in range(1,N+1):
+for i in range(1,n+1):
     if i%2==0:
         sum+=i
-print("The sum of all even numbers is ",sum)
+print("sum of all even number: ",sum)
