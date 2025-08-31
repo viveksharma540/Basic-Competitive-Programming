e2eb5d51fe46bc6414a5ef4f4741fc526a159e89
viveksharma540 <153981671+viveksharma540@users.noreply.github.com>
2025-08-31 07:20:08 +0530

    Delete Sheet2(9).py

--- Sheet2(9).py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter:\n"))
-sumofdigits = 0
-while n != 0:
-    digit = n % 10        
-    sumofdigits += digit   
-    n = n // 10           
-
-print("sum of all digits:", sumofdigits)
